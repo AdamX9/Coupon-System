84abebe9633b220835ae88d48415bc225aae88d7
AdamX9 <adam.moriou@gmail.com>
2022-08-24 19:27:25 +0300

    first commit

---
+++ CouponSystem Client-Side/couponsystem/src/Components/LayoutArea/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./Footer.css"
+
+function Copyright(): JSX.Element {
+    return (
+        <div className="copyright">
+           
+            {/* Copyright&copy; Shay and Adam */}
+        </div>
+    );
+}
+
+export default Copyright;
